cb658fbc908cef46edcf309cebdc93befe4a6e61
Vishesh Verma <106264088+vishesh1111@users.noreply.github.com>
2024-08-26 20:22:40 +0530

    Delete src/components/Experience directory

--- src/components/Experience/Experience.js
+++
@@ -1,8 +0,0 @@
-import React, { useEffect } from "react";
-import "./Experience.css";
-import Aos from "aos";
-import "aos/dist/aos.css";
-import facebook from "../assets/facebook.png";
-import google from "../assets/google.png";
-import quora from "../assets/quora.png";
-import airbnb from "../assets/airbnb.png";
